562af7b329c30091fddd0c12c78589ee5c7023bf
Jaya-supriya-somasani <somasanijayasupriya@gmail.com>
2022-03-16 17:51:39 +0530

    Python-Programs

---
+++ sequence_num2.py
@@ -0,0 +1,13 @@
+"""
+observe the data below. 
+	3 + 2 + 4 = 14
+	4 + 3 + 7 = 33
+	3 + 4 + 9 = 48
+	3 + 1 + 8 = 11
+	v1 + v2 + v3 = output
+Based on given data, take 3 values as input and print the expected output.
+"""
+v1=int(input())
+v2=int(input())
+v3=int(input())
+print(v1*v2+v2*v3)
