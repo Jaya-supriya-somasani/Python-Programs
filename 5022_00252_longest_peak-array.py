58946a2bde0d4d9820a05b3a167ef9dc93d0a1aa
Jaya Supriya Somasani <53718752+Jaya-supriya-somasani@users.noreply.github.com>
2022-04-01 16:45:24 +0530

    Add files via upload

---
+++ 5022_00252_longest_peak-array.py
@@ -0,0 +1,35 @@
+'''Write a function that takes in an array of integers and returns the length of the longest peak in the array.
+A peak is defined as adjacent integers in the array that are strictly increasing until they reach a tip (the highest value in the peak), at which point they become strictly decreasing. At least three integers are required to form a peak.
+For example, the integers 1, 4, 10, 2 form a peak, but the integers 4, 0, 10 don't and neither do the integers 1, 2, 2, 0. Similarly, the integers 1, 2, 3 don't form a peak because there aren't any strictly decreasing integers after the 3.
+
+Sample Input
+array = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
+
+Sample Output
+lenght :6 
+(elements : 0, 10, 6, 5, -1, -3)
+'''
+
+elements=list(map(int,input().split(" ")))
+def largePeak(elements):
+    i=1
+    pre_peaks=0
+    while(i<=len(elements)-2):
+        if(elements[i]>elements[i-1] and elements[i]>elements[i+1]):
+            count=1
+            j=i
+            while(j>=1 and elements[j]>elements[j-1]):
+                j=j-1
+                count=count+1
+
+            while(i<=len(elements)-2 and elements[i]>elements[i+1]):
+                i=i+1
+                count=count+1
+            pre_preaks=max(pre_peaks,count)
+        else:
+            i=i+1
+    return pre_preaks,i,j
+peak_count,ending_indx,starting_indx=largePeak(elements)
+print(peak_count)
+print(elements[starting_indx:ending_indx+1])
+
